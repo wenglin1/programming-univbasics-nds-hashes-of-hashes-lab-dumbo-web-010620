06067b302139faaf88112f5e289adc382af7fd36
weng lin <wenglin@buffalo.edu>
2019-12-20 05:28:42 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -4,42 +4,52 @@
 #
 # The tests will guide your construction.
 #
-# PORTION_1 = {
-#   label: "Kingdom",
-#   sub_category: {
-#     label: "Phylum",
-#     sub_category: {
-#       label: "Class"
-#     }
-#   }
-# }
-# 
-# PORTION_2 = {
-#   label: "Order"
-# }
-# 
-# PORTION_3 = {
-#   label: "Family",
-#   sub_category: {
-#     label: "Genus",
-#   }
-# }
-# 
-# PORTION_4 = {
-#   label: "Species",
-#   sub_category: nil
-# }
+PORTION_1 = {
+  label: "Kingdom",
+  sub_category: {
+    label: "Phylum",
+    sub_category: {
+      label: "Class"
+    }
+  }
+}
+
+PORTION_2 = {
+  label: "Order"
+}
+
+PORTION_3 = {
+  label: "Family",
+  sub_category: {
+    label: "Genus",
+  }
+}
+
+PORTION_4 = {
+  label: "Species",
+  sub_category: nil
+}
 
 def naming_system
-  # Remember:
-  #  Kingdom
-  #  Phylum
-  #  Class
-  #  Order
-  #  Family
-  #  Genus
-  #  Species
-  # So, if we have the "Kingdom" node we should be able to "tunnel" into the
-  # HoH all the way to Species!
-
+{:label => "Kingdom",
+  :sub_category => {
+      :label => "Phylum",
+      :sub_category => {
+        :label => "Class",
+        :sub_category => {
+          :label => "Order",
+          :sub_category => {
+            :label => "Family",
+            :sub_category => {
+              :label => "Genus",
+              :sub_category => {
+                :label => "Species",
+                :sub_category => nil
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 end
